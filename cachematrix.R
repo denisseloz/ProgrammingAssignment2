d6734bed08f8890089562fca9910aec1bb8a40b9
denisseloz <56004305+denisseloz@users.noreply.github.com>
2019-10-24 23:39:53 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,55 @@
 ## functions do
 
 ## Write a short comment describing this function
+## I set the input x as a matrix 
+## Set de "z" vallue as NULL
+## "mean' reference substituted by "solve" 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(sample(1:300, 10), 5,5)) {
+    z <- NULL
+   set <- function(y) { 
+           
+                     x <<- y 
+      
+                     Z<<- NULL 
+      
+    }
+    
+    
+     get<- function()x
+     setsolve  <- function(solve) z <<- solve
+     getsolve <- function() Z
+     list(set = set, get = get, 
+                    setsolve = setsolve, 
+                    getsolve = getsolve)
+    
 }
 
 
 ## Write a short comment describing this function
+## change reference "mean" by "solve"
+## change m for Z 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  z <- x$getsolve()
+  
+  if (!is.null(z)){
+    
+    message ("getting inverse matrix")
+    
+    return(z)
+    
+  }
+
+     data <- x$get()
+     
+    Z<- solve(data, ...)
+    
+    x$setsolve (z)
+    
+        z
+        
 }
+        ## Return a matrix that is the inverse of 'x'
+
